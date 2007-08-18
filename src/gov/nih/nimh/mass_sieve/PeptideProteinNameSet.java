31cda931ede5728b71281be86bab0a821b958a9b
slottad <slottad@gmail.com>
2007-08-18 03:26:02 +0000

    Updated javadoc
    git-svn-id: svn://www.proteomecommons.org/svn/MassSieve/dev@53 6cef8090-e22d-0410-a575-d54c6630a075

--- src/gov/nih/nimh/mass_sieve/PeptideProteinNameSet.java
+++ src/gov/nih/nimh/mass_sieve/PeptideProteinNameSet.java
@@ -12,7 +12,8 @@ package gov.nih.nimh.mass_sieve;
 import java.util.Set;
 
 /**
- *
+ * This class is used for display (in a tree) of complete sets of peptides and proteins.
+ * Because this is for display, only the names (strings) of the entities are stored.
  * @author slotta
  */
 public class PeptideProteinNameSet {
@@ -26,56 +27,48 @@ public class PeptideProteinNameSet {
     }
 
     /**
-     * 
-     * @return 
+     * Returns the stored set of peptide names
+     * @return Set of peptide strings
      */
     public Set<String> getPeptides() {
         return peptides;
     }
 
     /**
-     * 
-     * @param peptides 
+     * Stroes the set of petides strings.
+     * @param peptides Set of peptide strings
      */
     public void setPeptides(Set<String> peptides) {
         this.peptides = peptides;
     }
 
     /**
-     * 
-     * @return 
+     * returns a set of protein names.
+     * @return Set of Protein names
      */
     public Set<String> getProteins() {
         return proteins;
     }
 
     /**
-     * 
-     * @param proteins 
+     * Stores a set of protein names.
+     * @param proteins Set of protein names
      */
     public void setProteins(Set<String> proteins) {
         this.proteins = proteins;
     }
 
     /**
-     * 
-     * @return 
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * 
-     * @param name 
+     * Set the name of this protein and peptide collection
+     * @param name the name to be set
      */
     public void setName(String name) {
         this.name = name;
     }
     
     /**
-     * 
-     * @return 
+     * The name of this set of peptides and proteins
+     * @return set name.
      */
     public String toString() {
         return name;
